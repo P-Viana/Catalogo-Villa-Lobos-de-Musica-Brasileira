7783463fde8778a47093e3da77a59707028b19e7
P-Viana <viana.pedro.barbosa@gmail.com>
2024-10-03 15:53:37 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-function darkMode() {
-    var darkTheme = document.body;
-    darkTheme.classList.toggle("dark-mode");
- }
+var testa = 0;
+var darktheme = document.body;
+var tags = 0;
+
+function darkMode()
+{
+   if(testa == 0)
+   {
+    darktheme.classList.toggle("dark-mode");
+    tags = document.querySelectorAll('.tag');
+    tags.forEach
+    (tag => 
+        {tag.style.color = 'white';}
+    );
+    testa = 1;
+    tags = 1;
+   }
+   else
+   {
+    darktheme.classList.toggle("dark-mode");
+    tags = document.querySelectorAll('.tag');
+    tags.forEach
+    (tag => 
+        {tag.style.color = 'white';}
+    );
+    testa = 0;
+    tags = 0;
+   }
+}
